6252ed5dd988c9cda70144726fecefc1e8f57b13
Malorie Lara <malorielara@users.noreply.github.com>
2018-08-28 18:37:42 -0500

    added firebase info

---
+++ trains.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.4.1/firebase.js"></script>
+
+$(document)
+var config = {
+    apiKey: "AIzaSyBRBXjIwkFcoLU5dwMliZCr4M5ojAgN3Cg",
+    authDomain: "trainscheduler-7f4b4.firebaseapp.com",
+    databaseURL: "https://trainscheduler-7f4b4.firebaseio.com",
+    projectId: "trainscheduler-7f4b4",
+    storageBucket: "",
+    messagingSenderId: "763037232069"
+  };
+
+  firebase.initializeApp(config);
